be6fa067ce59b95769b5c9b7299b854c2ec1127a
zidein07 <zidein07@gmail.com>
2015-08-26 10:09:04 +0400

    get value from collection with help JSON!

--- myWork/oneApp/src/code.js
+++ myWork/oneApp/src/code.js
@@ -1,1 +1,25 @@
-console.log('console');
+var app = app || {};
+
+var MODEL = Backbone.Model.extend({
+	defaults: {
+		'title': '',
+		'status': 'active',
+		'check': false
+	}
+});
+
+var COLLECTION = Backbone.Collection.extend({
+	model: MODEL
+});
+
+var collection = new COLLECTION([{
+	title: 'hello'
+}]);
+
+console.log(collection.toJSON()[0].title);
+
+var VIEW = Backbone.View.extend({
+	
+});
+
+
